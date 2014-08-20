f0065e14b4006477353680a72ee20d3c78b2a6af
studentIvan <mail@studentivan.ru>
2014-08-20 22:51:07 +0400

    python ./manage.py ng_django_run

---
+++ sources/objects/management/commands/ng_django_run.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+from django.core.management.base import BaseCommand, CommandError
+from django.core import management
+from random import random
+import subprocess
+
+
+class Command(BaseCommand):
+    option_list = BaseCommand.option_list
+
+    def handle(self, *args, **options):
+        print('\033[0;43m\t\t\t\t\tgulp tasks\t\t\t\t'
+              '\t\033[0;37m \nrandom = %s\033[0;00m' % int(random() * 1e10))
+        subprocess.call("gulp", shell=True)
+        print('')
+
+        print('\033[0;42m\t\t\tdjango development server\t\t\t'
+              '\033[0;32m \nrandom = %s\033[0;00m' % int(random() * 1e10))
+        management.call_command('runserver')
