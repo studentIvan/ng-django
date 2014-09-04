e79129de4ef0ae03fbee2a9de4b1c08f5b6440da
studentIvan <mail@studentivan.ru>
2014-09-04 21:13:37 +0400

    pep8

--- sources/project/angular_urls.py
+++ sources/project/angular_urls.py
@@ -5,7 +5,7 @@ def parse(path):
     """
     parse angular.js routing file and return key routes
     """
-    routingFile = open(path, 'r')
-    jsBuffer = routingFile.read()
-    routingFile.close()
-    return tuple(set(re.findall(r'\.when\(\'/(\w+)', jsBuffer)))
+    routing_file = open(path, 'r')
+    js_buffer = routing_file.read()
+    routing_file.close()
+    return tuple(set(re.findall(r'\.when\(\'/(\w+)', js_buffer)))
